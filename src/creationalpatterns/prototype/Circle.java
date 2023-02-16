7dbc396bef01046e3facd26df3fc3110460bec80
IdrishanParlayan <idrishanp@hotmail.com>
2023-02-16 22:54:33 +0300

    Prototype design pattern wass added to project

---
+++ src/creationalpatterns/prototype/Circle.java
@@ -0,0 +1,29 @@
+package creationalpatterns.prototype;
+
+public class Circle extends Shape {
+    public int radius;
+    public Circle()
+    {}
+
+    public Circle(Circle target)
+    {
+        super(target);
+        if(target != null)
+            this.radius = target.radius;
+    }
+    @Override
+    public Shape clone()
+    {
+        return new Circle(this);
+    }
+
+    @Override
+    public boolean equals(Object object)
+    {
+        if(!(object instanceof  Circle) || !super.equals(object))
+            return false;
+
+        Circle circle = (Circle) object;
+        return this.radius == circle.radius;
+    }
+}
